7178ea9d8482393222e15fde0d9ff017ed432f16
Adrien Guillement <adrien.guillement@gmail.com>
2021-02-04 14:21:53 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,8 @@ echo ""
 
 for host in ${INPUT_HOSTS}; do
   echo -e "${BLUE}Connecting to ${host}...${NORMAL}"
-  echo "::set-output name=ssh-output::$(sh -c "ssh -q -t -i ~/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '${host}' -p ${INPUT_PORT} < ~/script.sh")\n"
+  sh -c "ssh -q -t -i ~/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '${host}' -p ${INPUT_PORT} < ~/script.sh" > /tmp/output
+  echo "::set-output name=ssh-output::$(cat /tmp/output)\n"
   echo ""
 done
 
